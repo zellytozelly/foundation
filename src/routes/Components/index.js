c0e192fd7adada34a1975646790ebbf740fc375f
zellytozelly <zellytozelly@gmail.com>
2022-05-08 12:19:57 +0900

    Feat: Add SwitchButton for Route

--- src/routes/Components/index.js
+++ src/routes/Components/index.js
@@ -1,17 +1,31 @@
 import { useState } from 'react'
 import styles from './Components.module.scss'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 
-import Input from './Input/Input'
 import Tab from './Tab/Tab'
 import Toggle from './Toggle/Toggle'
+import Slider from './Slider/Slider'
+import Input from './Input/Input'
+import Dropdown from './Dropdown/Dropdown'
+import SwitchButton from './SwitchButton/SwitchButton'
 
 function Components() {
-
   return (
     <div className={styles.componentsWrapper}>
-      <Toggle />
-      <Tab />
-      <Input />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ 
+            <>     
+              <Toggle />
+              <Tab />
+              <Slider />
+              <Input />
+              <SwitchButton />
+            </>
+          } />
+          <Route path='dropdown' element={ <Dropdown /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
